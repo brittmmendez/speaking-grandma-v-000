52f8bc995350b52d96ca165874fad9cb7c37069d
Brittany Morris <brittmmendez@gmail.com>
2017-11-29 15:15:13 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,6 +13,10 @@
 
 def speak_to_grandma(talk)
   if talk == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-    end
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif talk == talk.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
 end
