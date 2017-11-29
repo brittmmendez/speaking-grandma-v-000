2241efa30b40007e366574670e0597989391901c
Brittany Morris <brittmmendez@gmail.com>
2017-11-29 15:12:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,11 +12,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(talk)
-  if talk == talk.upcase
-    puts "NO, NOT SINCE 1938!"
+  if talk != talk.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
   elsif talk == "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    puts "NO, NOT SINCE 1938!"
   end
 end
